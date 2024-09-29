38dbf03533c1c26b0b166770f20f067d975652f3
SiljeW <silwal00204@stud.noroff.no>
2024-09-29 18:47:28 +0200

    Login update for form

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,14 +1,17 @@
 import { login } from "../../api/auth/login";
 
 export async function onLogin(event) {
-    event.preventDefault();
     const form = document.querySelector("#loginForm");
 
-    form.addEventListener("submit", (event) => {
-    const form = event.target;
-    const formData = new FormData(form);
-    const profile = Object.fromEntries(formData.entries());
-    })
-
-    login(profile);
+    if (form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const form = event.target;
+            const formData = new FormData(form);
+            const profile = Object.fromEntries(formData.entries());
+            })
+        
+            login(profile);
+    }
+    
 }
